b7ec02d498ae9a162afc2fd8d6490d19475364ef
Linus Lee <100479lee@gmail.com>
2016-11-28 01:57:40 -0500

    Added view details

--- js/views.js
+++ js/views.js
@@ -10,8 +10,7 @@ Ligature.viewManager = Ligature.models.Base.extend({
             activeBook: false,
             activeNote: false,
 
-            openBooks: [],
-            editingMode: false
+            editingMode: 'overview'
         };
     },
 
@@ -50,23 +49,55 @@ Ligature.views.Base = Backbone.View.extend({
 
 });
 
-Ligature.views.Header = Backbone.View.extend({
+Ligature.views.Sidebar = Backbone.View.extend({
 
-    tagName: 'header',
-    className: '',
+    className: 'sidebar',
+    header: false,
+    booklist: false,
 
-    template: `
-      <div></div>
-    `,
+    template: '',
 
     events: {},
 
     initialize: function(attrs) {
+        this.header = new Ligature.views.Header();
+        this.booklist = new Ligature.views.BookList();
+
         this.render();
     },
 
     render: function() {
         this.$el.html(this.template);
+        this.$el.append(this.header.render().$el);
+        this.$el.append(this.booklist.render().$el);
+
+        return this;
+    }
+
+});
+
+Ligature.views.Header = Backbone.View.extend({
+
+    tagName: 'header',
+    toolbar: false,
+
+    template: `
+      <a href="/">
+        Ligature
+      </a>
+    `,
+
+    events: {},
+
+    initialize: function(attrs) {
+        this.toolbar = new Ligature.views.Toolbar();
+        this.render();
+    },
+
+    render: function() {
+        this.$el.html(this.template);
+        this.$el.append(this.toolbar.render().$el);
+
         return this;
     }
 
@@ -77,10 +108,37 @@ Ligature.views.Toolbar = Backbone.View.extend({
     className: 'toolbar',
 
     template: `
-      <div></div>
+      <div>
+        <button class="add">add</button>
+        <button class="collapse">collapse</button>
+        <button class="search">search</button>
+
+        <input class="searchbar" type="text" placeholder="search"></input>
+      </div>
     `,
 
-    events: {},
+    events: {
+        'click .add': 'addClick',
+        'click .collapse': 'collapseClick',
+        'click .search': 'searchClick',
+        'keydown .searchbar': 'searchInput'
+    },
+
+    addClick: function(evt) {
+
+    },
+
+    collapseClick: function(evt) {
+
+    },
+
+    searchClick: function(evt) {
+
+    },
+
+    searchInput: function(evt) {
+
+    },
 
     initialize: function(attrs) {
         this.render();
@@ -95,20 +153,25 @@ Ligature.views.Toolbar = Backbone.View.extend({
 
 Ligature.views.BookList = Backbone.View.extend({
 
-    className: '',
+    className: 'booklist',
+    books: false,
 
-    template: `
-      <div></div>
-    `,
+    template: '',
 
     events: {},
 
     initialize: function(attrs) {
+        this.books = Ligature.objects.books.map(bookModel =>
+            new Ligature.views.BookListItem({ model: bookModel })
+        );
+
         this.render();
     },
 
     render: function() {
         this.$el.html(this.template);
+        this.books.forEach(bookView => this.$el.append(bookView.render().$el));
+
         return this;
     }
 
@@ -116,41 +179,45 @@ Ligature.views.BookList = Backbone.View.extend({
 
 Ligature.views.BookListItem = Backbone.View.extend({
 
-    className: '',
+    className: 'booklistItem',
+    expanded: false,
+    notelist: false,
 
     template: `
-      <div></div>
+      <div class="bookname">
+      </div>
+
+      <div class="lastEdited">
+      </div>
+      <div class="noteCount">
+      </div>
+
+      <div class="notes expanded${this.expanded}">
+      </div>
     `,
 
-    events: {},
+    events: {
+        'click': 'select'
+    },
 
     initialize: function(attrs) {
+        this.notelist = new Ligature.views.NoteList({ model: this.model });
+        this.render();
+    },
+
+    select: function(evt) {
+        this.expanded = !this.expanded;
         this.render();
     },
 
     render: function() {
         this.$el.html(this.template);
-        return this;
-    }
 
-});
+        this.$('.bookname').html(this.model.get('name'));
+        this.$('.lastEdited').html(this.model.get('notes').first().get('timestamp'));
+        this.$('.noteCount').html(this.model.get('notes').length);
+        this.$el.append(this.notelist.render().$el);
 
-Ligature.views.BookDropdown = Backbone.View.extend({
-
-    className: '',
-
-    template: `
-      <div></div>
-    `,
-
-    events: {},
-
-    initialize: function(attrs) {
-        this.render();
-    },
-
-    render: function() {
-        this.$el.html(this.template);
         return this;
     }
 
@@ -158,20 +225,25 @@ Ligature.views.BookDropdown = Backbone.View.extend({
 
 Ligature.views.NoteList = Backbone.View.extend({
 
-    className: '',
+    className: 'notelist',
+    notes: false,
 
-    template: `
-      <div></div>
-    `,
+    template: '',
 
     events: {},
 
     initialize: function(attrs) {
+        this.notes = Ligature.objects.notes.map(noteModel =>
+            new Ligature.views.NoteListItem({ model: noteModel })
+        );
+
         this.render();
     },
 
     render: function() {
         this.$el.html(this.template);
+        this.books.forEach(noteView => this.$el.append(noteView.render().$el));
+
         return this;
     }
 
@@ -179,34 +251,59 @@ Ligature.views.NoteList = Backbone.View.extend({
 
 Ligature.views.NoteListItem = Backbone.View.extend({
 
-    className: '',
+    className: 'notelistItem',
+    buttonset: false,
 
     template: `
-      <div></div>
+      <div class="title">
+      </div>
+      <div class="firstline">
+      </div>
+
+      <div class="buttonset">
+      </div>
     `,
 
-    events: {},
+    events: {
+        'click': 'select'
+    },
 
     initialize: function(attrs) {
+        this.buttonset = new Ligature.views.NoteButtonSet();
         this.render();
+    },
+
+    select: function(evt) {
+        Ligature.viewManager.set('activeNote', this.model);
     },
 
     render: function() {
         this.$el.html(this.template);
+
+        this.$('.title').html(this.model.get('title'));
+        this.$('.firstline').html(this.model.get('content').substr(0, 100));
+        this.$('.buttonset').html(this.buttonset.render().$el);
+
         return this;
     }
 
 });
 
-Ligature.views.Sidebar = Backbone.View.extend({
+Ligature.views.NoteButtonSet = Backbone.View.extend({
 
-    id: 'sidebar',
+    className: 'notebuttonset',
 
     template: `
-      <div></div>
+      <button class="back">back</button>
+      <button class="copy">copy</button>
+      <button class="delete">delete</button>
     `,
 
-    events: {},
+    events: {
+        'click .back': 'backClick',
+        'click .copy': 'copyClick',
+        'click .delete': 'deleteClick'
+    },
 
     initialize: function(attrs) {
         this.render();
@@ -222,41 +319,51 @@ Ligature.views.Sidebar = Backbone.View.extend({
 Ligature.views.Editor = Backbone.View.extend({
 
     id: 'editor',
+    buttonset: false,
 
     template: `
-      <div></div>
+      <button class='modeSwitch'>mode</button>
+      <div class="buttonset">
+      </div>
+
+      <div class="titlebar">
+      </div>
+
+      <div class="metadatabar">
+        <span class="lastEdited"></span> |
+        <span class="words"></span> |
+        <span class="chars"></span>
+      </div>
+
+      <textarea class="content">
+      </textarea>
     `,
 
-    events: {},
+    events: {
+        'click .modeSwitch': 'modeSwitch',
+        'click .titlebar': 'setTitle'
+    },
 
     initialize: function(attrs) {
+        this.buttonset = new Ligature.views.NoteButtonSet();
+
         this.render();
     },
 
     render: function() {
         this.$el.html(this.template);
-        return this;
-    }
 
-});
+        const activeNote = Ligature.viewManager.get('activeNote');
 
-Ligature.views.ModeButton = Backbone.View.extend({
+        this.$('.titlebar').html(activeNote.get('title'));
+        this.$('.content').val(activeNote.get('content'));
 
-    tagName: 'button',
-    id: 'mode-button',
+        this.$('.lastEdited').html(activeNote.get('timestamp'));
+        this.$('.words').html(activeNote.get('content').split(' ').length);
+        this.$('.chars').html(activeNote.get('content').length);
 
-    template: `
-      <div></div>
-    `,
+        this.$('.buttonset').html(this.buttonset.render().$el);
 
-    events: {},
-
-    initialize: function(attrs) {
-        this.render();
-    },
-
-    render: function() {
-        this.$el.html(this.template);
         return this;
     }
 
@@ -267,13 +374,46 @@ Ligature.views.Modal = Backbone.View.extend({
     id: 'modal',
 
     template: `
-      <div></div>
+      <div class="copy">
+      </div>
+
+      <input class="modalInput"></input>
+
+      <button class="bad">Cancel</button>
+      <button class="good">Ok</button>
     `,
 
-    events: {},
+    events: {
+        'click .good': 'goodClick',
+        'click .bad': 'badClick'
+    },
 
     initialize: function(attrs) {
         this.render();
+    },
+
+    goodClick: function(evt) {
+
+    },
+
+    badClick: function(evt) {
+
+    },
+
+    alert: function(options) {
+
+    },
+
+    confirm: function(options) {
+
+    },
+
+    prompt: function(options) {
+
+    },
+
+    _call: function(copy, showInput, showBad, goodCallback, badCallback) {
+
     },
 
     render: function() {
@@ -285,18 +425,25 @@ Ligature.views.Modal = Backbone.View.extend({
 
 Ligature.views.App = Backbone.View.extend({
 
-    template: `
-      <div></div>
-    `,
+    template: '',
+    sidebar: false,
+    editor: false,
 
     events: {},
 
     initialize: function(attrs) {
+        this.sidebar = new Ligature.views.Sidebar();
+        this.editor = new Ligature.views.Editor();
+        this.modal = new Ligature.views.Modal();
         this.render();
     },
 
     render: function() {
         this.$el.html(this.template);
+        this.$el.append(this.sidebar.render().$el);
+        this.$el.append(this.editor.render().$el);
+        this.$el.append(this.modal.render().$el);
+
         return this;
     }
 
